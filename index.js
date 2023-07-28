1d2e3de951c726b88c21df0766dd07dc70390de8
Aviza Anandya <75213952+vizaanandyaa@users.noreply.github.com>
2023-07-28 07:40:01 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const app = express();
+const Video = require('./models/VideoThumbnailList');
+const Product = require('./models/ProductList');
+const Comment = require('./models/CommentList')
+app.use(
+    bodyParser.urlencoded({
+        extended:true,
+    }),
+);
+
+app.get('/video/:videoId', async (req, res)=>{
+    try{
+        const videos = await Video.findById(req.params.id);
+        res.json(videos)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.get('/product/:videoId', async (req, res)=>{
+    try{
+        const products = await Product.findById(req.params.id);
+        res.json(products)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.get('/comment/:videoId', async (req, res)=>{
+    try{
+        const comments = await Comment.findById(req.params.id);
+        res.json(comments)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.post('/postComment',(req,res)=>{
+    const postComments = new Comment({
+        username: req.body.username,
+        comment: req.body.comment,
+        videoId: req.body.videoId
+    })
+
+    try{
+        const commentToSave = postComments.save();
+        res.status(200).json(commentToSave)
+    }
+
+    catch(error){
+        res.status(400).json({message:error.message})
+    }
+})
+
+
+
+
+
+
+
+app.use(bodyParser.json());
+
+
+app.listen(3000,()=>console.log('Listening on port 3000'))
